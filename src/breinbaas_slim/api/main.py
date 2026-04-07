faa47c89045e13ae00307c9e67ec990d5c88974e
Rob van Putten <breinbaasnl@gmail.com>
2026-04-07 14:15:43 +0000

    added cors

--- src/breinbaas_slim/api/main.py
+++ src/breinbaas_slim/api/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 
 from .endpoints import router
 
@@ -9,5 +9,16 @@ app = FastAPI(
     description="API for parsing CPT and Borehole data, and interpreting Soil Profiles.",
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://breinbaas.nl",
+        "https://api.breinbaas.nl",
+        "*",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router, prefix="/api")
